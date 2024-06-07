b49855061510d1fd7ba2c53eaf5f591208822d84
francisco-24-0658 <franciscopsuero@gmail.com>
2024-06-07 18:36:35 -0400

    Add files via upload

---
+++ calculadoe de masa corporal.py
@@ -0,0 +1,6 @@
+#calculador de masa colporal
+print("calcule el indice de masa corporal")
+peso = float(input("ingrese su peso en kg: "))
+Artura = float(input("ingrese su altura en m: "))
+imc = peso / (Artura ** 2)
+print("tu indice de masa corporal es:", imc) 
